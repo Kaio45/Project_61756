c573148134ac68029eecfe4f6856476c8c13b347
Tehila-BD <tehilabd73@gmail.com>
2026-01-16 16:49:05 +0200

    Add Javadoc comments to ConnectionToClient class
    Added documentation comments to the ConnectionToClient class and its methods.

--- Assignment_3/BistroServer/src/ocsf/server/ConnectionToClient.java
+++ Assignment_3/BistroServer/src/ocsf/server/ConnectionToClient.java
@@ -2,15 +2,38 @@ package ocsf.server;
 
 import java.io.*;
 import java.net.*;
-import java.util.*;
 
+/**
+ * An instance of this class is created for each connection to a client.
+ * It handles the input and output streams for a specific client connection.
+ * @author Group-17
+ * @version 1.0
+ */
 public class ConnectionToClient extends Thread {
+  
+  /** The server instance to which this connection belongs. */
   private AbstractServer server;
+  
+  /** The socket connecting to the client. */
   private Socket clientSocket;
+  
+  /** The stream used to read from the client. */
   private ObjectInputStream input;
+  
+  /** The stream used to write to the client. */
   private ObjectOutputStream output;
+  
+  /** Indicates if the connection loop should stop. */
   private boolean readyToStop;
 
+  /**
+   * Constructs a new connection to a client.
+   *
+   * @param group the thread group that contains the connections.
+   * @param clientSocket the socket contains the client's connection.
+   * @param server a reference to the server that created this instance.
+   * @throws IOException if an I/O error occur when creating the streams.
+   */
   ConnectionToClient(ThreadGroup group, Socket clientSocket, AbstractServer server) throws IOException {
     super(group, (Runnable)null);
     this.clientSocket = clientSocket;
@@ -26,11 +49,21 @@ public class ConnectionToClient extends Thread {
     this.start();
   }
 
+  /**
+   * Sends an object to the client.
+   *
+   * @param msg the message to be sent.
+   * @throws IOException if an I/O error occur when sending the message.
+   */
   public final void sendToClient(Object msg) throws IOException {
     if (clientSocket == null || output == null) throw new SocketException("socket does not exist");
     output.writeObject(msg);
   }
 
+  /**
+   * Constantly reads the client's input stream.
+   * Receives messages and forwards them to the server's message handler.
+   */
   public void run() {
     Object msg;
     try {
@@ -52,16 +85,31 @@ public class ConnectionToClient extends Thread {
     }
   }
 
+  /**
+   * Closes the connection to the client.
+   *
+   * @throws IOException if an error occurs when closing the socket.
+   */
   public void close() throws IOException {
     readyToStop = true;
     try { closeAll(); } catch (Exception ex) { }
   }
 
+  /**
+   * Closes all streams and the socket.
+   *
+   * @throws IOException if an I/O error occurs.
+   */
   private void closeAll() throws IOException {
     if (clientSocket != null) clientSocket.close();
     if (output != null) output.close();
     if (input != null) input.close();
   }
   
+  /**
+   * Returns the address of the client.
+   *
+   * @return the InetAddress of the client.
+   */
   public InetAddress getInetAddress() { return clientSocket == null ? null : clientSocket.getInetAddress(); }
 }

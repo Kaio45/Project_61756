5a108259b9c363013caf2b5c4b1854254c070710
Tehila-BD <tehilabd73@gmail.com>
2026-01-16 16:53:50 +0200

    Refactor Message class with improved documentation
    Updated class documentation and added author/version information.

--- Assignment_3/BistroClient/src/common/Message.java
+++ Assignment_3/BistroClient/src/common/Message.java
@@ -3,15 +3,27 @@ package common;
 import java.io.Serializable;
 
 /**
- * Represents a generic message sent between the Client and the Server.
+ * Represents a generic message wrapper for communication between Client and Server.
+ * Contains an action type and a payload object.
+ * @author Group-17
+ * @version 1.0
  */
 public class Message implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /** The action to be performed. */
     private ActionType action;
+    
+    /** The data associated with the action (can be null). */
     private Object content;
 
+    /**
+     * Constructs a new Message.
+     *
+     * @param action the action type
+     * @param content the message content/payload
+     */
     public Message(ActionType action, Object content) {
         this.action = action;
         this.content = content;

3f18515b769b791f7c0f2f19448b2fbcf8ccb9c8
Tehila-BD <tehilabd73@gmail.com>
2026-01-16 16:53:04 +0200

    Expand ActionType enum with additional actions

--- Assignment_3/BistroClient/src/common/ActionType.java
+++ Assignment_3/BistroClient/src/common/ActionType.java
@@ -1,19 +1,34 @@
 package common;
 
 /**
- * Enum defining all possible actions/commands that can be sent between the Client and the Server.
+ * Enumeration of all possible actions/commands that can be sent between Client and Server.
+ * @author Group-17
+ * @version 1.0
  */
 public enum ActionType {
-    
-    // User / Login Actions
     LOGIN,
-    LOGOUT,
-    
-    // Order Actions
-    GET_ORDER,
     ADD_ORDER,
+    GET_ORDER,
     UPDATE_ORDER,
-    
-    GET_ALL_TABLES,       // Client requests the list of all tables
-    CHECK_AVAILABILITY    // Optional: Explicit check for availability
+    DELETE_ORDER,
+    REGISTER_SUBSCRIBER,
+    IDENTIFY_SUBSCRIBER,
+    IDENTIFY_BY_CODE,
+    GET_SUBSCRIBER_LAST_ORDER,
+    MARK_ARRIVED,
+    MARK_FINISHED,
+    GET_ALL_TABLES,
+    GET_REPORT,
+    UPDATE_SUBSCRIBER_DETAILS,
+    ADD_TABLE,
+    DELETE_TABLE,
+    UPDATE_OPENING_HOURS,
+    RECOVER_SUBSCRIBER_ID,
+    GET_ALL_ORDERS,
+    UPDATE_TABLE,
+    GET_DAILY_REPORT,
+    GET_WAITING_LIST,
+    GET_HISTORY_BY_USER_ID,
+    GET_SUBSCRIBER_DETAILS,
+    GET_APPROVED_ORDERS_FOR_TODAY
 }

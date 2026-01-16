96eb96bc4cb82b963155246aafdd26e5f4906325
YaelWroblevsky <yaelywro10@gmail.com>
2026-01-16 16:54:16 +0200

    Update ChatClient.java

--- Assignment_3/BistroClient/src/client/ChatClient.java
+++ Assignment_3/BistroClient/src/client/ChatClient.java
@@ -8,10 +8,9 @@ import javafx.application.Platform;
 
 /**
  * The ChatClient is responsible for handling communication between the client GUI and the server.
- * <p>
- * It extends {@link AbstractClient} and overrides methods to handle incoming messages
- * and connection events. It routes messages to the appropriate GUI controller.
- * </p>
+ * It routes incoming messages to the appropriate GUI controller (Login, Order, Report, etc.).
+ * @author Group-17
+ * @version 1.0
  */
 public class ChatClient extends AbstractClient {
   
@@ -21,56 +20,84 @@ public class ChatClient extends AbstractClient {
     /** Static reference to the LoginFrameController to update the login UI. */
     public static LoginFrameController loginController;
     
+    /** Static reference to the RegisterController. */
+    public static RegisterController registerController;
+    
+    /** Static reference to the ReportController. */
+    public static ReportController reportController; 
+    
+    /** Indicates if we are waiting for a response (optional usage). */
+    public static boolean awaitResponse = false;
+
     /**
      * Constructs an instance of the chat client.
      *
-     * @param host The server to connect to.
-     * @param port The port number to connect on.
-     * @throws IOException if an I/O error occurs when opening the connection
+     * @param host the server to connect to
+     * @param port the port number to connect on
+     * @throws IOException if an I/O error occurs during connection
      */
     public ChatClient(String host, int port) throws IOException {
-        super(host, port); // Call the superclass constructor
-        openConnection();  // Open the connection immediately
+        super(host, port); 
+        openConnection();  
     }
 
     /**
-     * Handles incoming messages from the server.
-     * <p>
-     * Checks the {@link ActionType} of the message and forwards it to the
-     * relevant controller (Login or Order).
-     * </p>
+     * Handles a message received from the server.
+     * Routes the message to the correct controller based on the ActionType.
      *
-     * @param msg The message received from the server.
+     * @param msg the message received from the server
      */
     @Override
     protected void handleMessageFromServer(Object msg) {
         System.out.println("Server says: " + msg);
+        awaitResponse = false;
         
         if (msg instanceof Message) {
             Message message = (Message) msg;
+            ActionType type = message.getAction();
+
+            // 1. Login and Identification
+            if (type == ActionType.LOGIN || type == ActionType.IDENTIFY_SUBSCRIBER || type == ActionType.IDENTIFY_BY_CODE) {
+                if (loginController != null) loginController.handleResponse(message);
+            }
             
-            // Route message based on ActionType
-            if (message.getAction() == ActionType.LOGIN) {
-                if (loginController != null) {
-                    loginController.handleLoginResponse(message);
+            // 2. Subscriber Registration
+            else if (type == ActionType.REGISTER_SUBSCRIBER) {
+                if (registerController != null) {
+                    registerController.handleResponse(message);
                 }
-            } else {
-                // Default handling for order-related actions
-                if (orderController != null) {
-                    orderController.updateFields(message);
+            }
+            
+            // 3. Reports
+            else if (type == ActionType.GET_REPORT) {
+                if (reportController != null) {
+                    reportController.updateReport((String) message.getContent());
                 }
+            }
+            
+            // 4. Recover Subscriber ID
+            else if (type == ActionType.RECOVER_SUBSCRIBER_ID) {
+                if (ForgotIdController.instance != null) {
+                    ForgotIdController.instance.displayResult(message.getContent());
+                }
+            }
+            
+            // 5. Default (Order Updates)
+            else {
+                if (orderController != null) orderController.updateFields(message);
             }
         }
     }
   
     /**
-     * Handles messages coming from the UI to be sent to the server.
+     * Handles messages coming from the client UI to be sent to the server.
      *
-     * @param message The message from the UI.
+     * @param message the message object to send
      */
     public void handleMessageFromClientUI(Object message) {
         try {
             sendToServer(message);
+            awaitResponse = true;
         } catch (IOException e) {
             System.out.println("Could not send message to server. Terminating client.");
             quit();
@@ -87,20 +114,12 @@ public class ChatClient extends AbstractClient {
         System.exit(0);
     }
 
-    /**
-     * Called when the connection to the server is closed.
-     */
     @Override
     protected void connectionClosed() {
         System.out.println("Server connection closed. Exiting...");
         Platform.runLater(() -> System.exit(0));
     }
 
-    /**
-     * Called when a connection exception occurs.
-     *
-     * @param exception the exception that occurred
-     */
     @Override
     protected void connectionException(Exception exception) {
         System.out.println("Server connection exception. Exiting...");
